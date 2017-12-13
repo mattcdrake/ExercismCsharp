359420e295ddf15e17cbb4ec2df777687ad254c1
Matt Drake <mcdtwb@gmail.com>
2017-12-13 14:00:43 -0500

    Completed reverse string

---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        Stack inChars = new Stack();
+        StringBuilder sb = new StringBuilder();
+
+        if (String.IsNullOrEmpty(input))
+        {
+            return "";
+        }
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            inChars.Push(input[i]);
+        }
+
+        while (inChars.Count != 0)
+        {
+            sb.Append(inChars.Pop());
+        }
+
+        return sb.ToString();
+    }
+
+}
