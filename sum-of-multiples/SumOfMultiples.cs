baf3b25a463130a9c40c5b474299a80ef98d88f0
Matt Drake <mcdtwb@gmail.com>
2017-12-13 18:02:55 -0500

    Finished SumOfMultiples

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        int sum = 0;
+        bool flagVal;
+
+        for (int curr = 0; curr < max; curr++)
+        {
+            flagVal = false;
+
+            foreach (int e in multiples)
+            {
+                if (curr % e == 0)
+                {
+                    flagVal = true;
+                }
+            }
+
+            if (flagVal)
+            {
+                sum += curr;
+            }
+        }
+
+        return sum;
+    }
+}
