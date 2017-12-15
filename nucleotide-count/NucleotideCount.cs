7a17562305341b5c9f95dc3f735d42229746599b
Matt Drake <mcdtwb@gmail.com>
2017-12-14 20:03:25 -0500

    Finished nucleotide count (finally)

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -1,17 +1,37 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public class NucleotideCount
 {
+    private Dictionary<char, int> dict = new Dictionary<char, int>()
+        {
+            {'A', 0 },
+            {'C', 0 },
+            {'G', 0 },
+            {'T', 0 }
+        };
+
     public NucleotideCount(string sequence)
     {
+        foreach (char c in sequence)
+        {
+            if (!dict.ContainsKey(c))
+            {
+                throw new InvalidNucleotideException();
+            }
+            else
+            {
+                dict[c]++;
+            }
+        }
     }
 
     public IDictionary<char, int> NucleotideCounts
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            return dict;
         }
     }
 }
