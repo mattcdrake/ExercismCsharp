8383bdb0c12587f1399bda0a6a4154f7941b3b93
Matt Drake <mcdtwb@gmail.com>
2017-12-16 17:54:32 -0500

    Finished Grade School (the exercise)

--- grade-school/GradeSchool.cs
+++ grade-school/GradeSchool.cs
@@ -1,20 +1,42 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public class School
 {
+    private List<Student> s_list = new List<Student>();
+
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        s_list.Add(new Student(student, grade));
     }
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        IEnumerable<string> output = from Student in s_list
+                                     orderby Student.grade, Student.name
+                                     select Student.name;
+        return output;
     }
 
     public IEnumerable<string> Grade(int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        IEnumerable<string> output = from Student in s_list
+                                     where Student.grade == grade
+                                     orderby Student.name
+                                     select Student.name;
+        return output;
+    }
+}
+
+public class Student
+{
+    public string name { get; set; }
+    public int grade { get; set; }
+
+    public Student(string name, int grade)
+    {
+        this.name = name;
+        this.grade = grade;
     }
 }
