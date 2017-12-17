3fb1d09f1a59a865c414d56a5ef2b22097fd21a0
Matt Drake <mcdtwb@gmail.com>
2017-12-17 12:43:56 -0500

    Finished Armstrong numbers

--- armstrong-numbers/ArmstrongNumbers.cs
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<int> digits = new List<int>();
+        int working_num = number;
+
+        while (working_num >= 10)
+        {
+            digits.Add(working_num % 10);
+            working_num /= 10;
+        }
+
+        digits.Add(working_num);
+
+        int actual = 0;
+
+        foreach(int i in digits)
+        {
+            actual += (int) Math.Pow(i, digits.Count);
+        }
+
+        return (actual == number);
     }
 }
