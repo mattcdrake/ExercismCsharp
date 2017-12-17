f618a4938bbbff65fba6b7d941803a7948db1488
Matt Drake <mcdtwb@gmail.com>
2017-12-17 17:40:38 -0500

    Finished Kindergarten garden

--- kindergarten-garden/KindergartenGarden.cs
+++ kindergarten-garden/KindergartenGarden.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+
 
 public enum Plant
 {
@@ -10,17 +12,64 @@ public enum Plant
 }
 
 public class KindergartenGarden
-{    
+{
+    Plant[] pots;
+    string[] students = new string[] { "Alice", "Bob", "Charlie", "David", "Eve", "Fred",
+                                        "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"};
+
     public KindergartenGarden(string diagram)
     {
-    }
+        diagram = diagram.Replace("\n", "");
+        pots = new Plant[diagram.Length];
 
-    public KindergartenGarden(string diagram, IEnumerable<string> students)
-    {
+        for (int i = 0; i < diagram.Length; i++)
+        {
+            if (diagram[i] == 'V')
+            {
+                pots[i] = Plant.Violets;
+            }
+            else if (diagram[i] == 'R')
+            {
+                pots[i] = Plant.Radishes;
+            }
+            else if (diagram[i] == 'C')
+            {
+                pots[i] = Plant.Clover;
+            }
+            else if (diagram[i] == 'G')
+            {
+                pots[i] = Plant.Grass;
+            }
+        }
     }
 
     public IEnumerable<Plant> Plants(string student)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<Plant> outval = new List<Plant>();
+        int pos, first_half, second_half;
+
+        try
+        {
+            pos = Array.IndexOf(students, student);
+            first_half = 2 * pos;
+            second_half = (pots.Length / 2) + first_half;
+        }
+        catch
+        {
+            throw new Exception("Give a valid student, dude.");
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            outval.Add(pots[first_half]);
+            first_half++;
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            outval.Add(pots[second_half]);
+            second_half++;
+        }
+
+        return outval;
     }
 }
