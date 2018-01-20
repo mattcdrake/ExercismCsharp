7239ac2f83f2311cc3ed24f29f27bfce273e44fc
mattcdrake <drake.mattc@gmail.com>
2018-01-20 12:49:23 -0500

    Finished SaddlePoints

--- saddle-points/SaddlePoints.cs
+++ saddle-points/SaddlePoints.cs
@@ -1,15 +1,53 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 public class SaddlePoints
 {
+    private int[,] matrix { get; set; }
+
     public SaddlePoints(int[,] values)
     {
+        matrix = values;
     }
 
     public IEnumerable<Tuple<int, int>> Calculate()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<Tuple<int, int>> points = new List<Tuple<int, int>>();
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (IsSaddle(i, j))
+                {
+                    points.Add(new Tuple<int, int>(i, j));
+                }
+            }
+        }
+        return points;
+    }
+
+    private bool IsSaddle(int y, int x) 
+    {
+        // Search matrix horizontally
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            if (matrix[y, i] > matrix[y, x])
+            {
+                return false;
+            }
+        }
+
+        // Search matrix vertically
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            if (matrix[i, x] < matrix[y, x])
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
