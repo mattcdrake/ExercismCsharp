731e7d27e1d7069c041e3f4fb86a123ee6916895
mattcdrake <drake.mattc@gmail.com>
2018-01-27 21:18:52 -0500

    Finished RnaTranscription

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 csharp
+
+### exercises to go back to
+1) refactoring
+2) bookstore
--- rna-transcription/RnaTranscription.cs
+++ rna-transcription/RnaTranscription.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Text;
 
 public static class RnaTranscription
 {
     public static string ToRna(string nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        StringBuilder rna = new StringBuilder("");
+
+        foreach (char item in nucleotide)
+        {
+            switch (item)
+            {
+                case 'G':
+                    rna.Append("C");
+                    break;
+                case 'C':
+                    rna.Append("G");
+                    break;
+                case 'T':
+                    rna.Append("A");
+                    break;
+                case 'A':
+                    rna.Append("U");
+                    break;                
+                default:
+                    break;
+            }
+        }
+        return rna.ToString();
     }
 }
