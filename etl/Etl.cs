6b3b9da5b75e99d2cb1f326b12b1622e5251acb6
mattcdrake <drake.mattc@gmail.com>
2018-01-30 19:23:58 -0500

    Finished ETL

--- etl/Etl.cs
+++ etl/Etl.cs
@@ -5,6 +5,16 @@ public static class Etl
 {
     public static IDictionary<string, int> Transform(IDictionary<int, IList<string>> old)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Dictionary<string, int> afterDict = new Dictionary<string, int>(); 
+
+        foreach(var item in old) 
+        {
+            foreach (var oldLetter in item.Value)
+            {
+                afterDict.Add(oldLetter.ToLower(), item.Key);
+            }
+        }
+
+        return afterDict;
     }
 }
